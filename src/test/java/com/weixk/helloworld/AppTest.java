046fc6085301d7adf2de22941c5599cfea59df06
weixiaokang <weixiaokang@freething.cn>
2016-12-21 22:11:21 +0800

    使用SpringRunner

--- src/test/java/com/weixk/helloworld/AppTest.java
+++ src/test/java/com/weixk/helloworld/AppTest.java
@@ -1,24 +1,27 @@
 package com.weixk.helloworld;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * Unit test for simple Application.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTest
 {
+    private static final Logger log = LoggerFactory.getLogger(AppTest.class);
     @Autowired
     private Environment environment;
     @Test
     public void test() {
         System.out.println(environment.getProperty("me.test.str"));
+        log.error("测试log日志打印！");
     }
 }
