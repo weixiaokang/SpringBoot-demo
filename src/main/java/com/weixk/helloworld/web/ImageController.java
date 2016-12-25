9bea9537704d4e009c573cf462d7604303849b30
weixiaokang <weixkz@gmail.com>
2016-12-25 23:49:35 +0800

    多文件上传

--- src/main/java/com/weixk/helloworld/web/ImageController.java
+++ src/main/java/com/weixk/helloworld/web/ImageController.java
@@ -5,10 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.List;
 
 /**
  * 上传下载文件
@@ -21,27 +24,29 @@ public class ImageController {
     private static final Logger log = LoggerFactory.getLogger(ImageController.class);
     private static final String IMG_PATH = "/Users/www1/img/";
     private static final String SERVER = "http://127.0.0.1:8080";
-//    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-//    public Result<String> uploadImg(HttpServletRequest request) {
-//        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("file");
-//        BufferedOutputStream stream = null;
-//        StringBuilder builder = new StringBuilder();
-//        for (MultipartFile file : files) {
-//            if (file.isEmpty()) continue;
-//            try {
-//                String fileName = IMG_PATH + file.getOriginalFilename();
-//                builder.append(SERVER).append("/download").append(fileName).append(";");
-//                byte[] bytes = file.getBytes();
-//                stream = new BufferedOutputStream(new FileOutputStream(new File(fileName)));
-//                stream.write(bytes);
-//                stream.flush();
-//                stream.close();
-//            } catch (IOException e) {
-//                return new Result<String>(0, "上传图片失败", e.getMessage());
-//            }
-//        }
-//        return new Result<String>(1, "上传图片成功", builder.toString());
-//    }
+
+    @RequestMapping(value = "/upload-multi", method = RequestMethod.POST)
+    public Result<String> uploadMultiImg(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("file");
+        BufferedOutputStream stream = null;
+        StringBuilder builder = new StringBuilder();
+        for (MultipartFile file : files) {
+            if (file.isEmpty()) continue;
+            try {
+                String path = file.getOriginalFilename() + "_" + System.currentTimeMillis();
+                String fileName = IMG_PATH + path;
+                builder.append(SERVER).append("/img/download/").append(path).append(";");
+                byte[] bytes = file.getBytes();
+                stream = new BufferedOutputStream(new FileOutputStream(new File(fileName)));
+                stream.write(bytes);
+                stream.flush();
+                stream.close();
+            } catch (IOException e) {
+                return new Result<>(0, "上传图片失败", e.getMessage());
+            }
+        }
+        return new Result<>(1, "上传图片成功", builder.toString());
+    }
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public Result<String> uploadImg(@RequestParam(value = "file") MultipartFile file) throws IOException {
@@ -51,9 +56,9 @@ public class ImageController {
             outputStream.write(file.getBytes());
             outputStream.flush();
             outputStream.close();
-            return new Result<String>(1, "图片上传成功", SERVER + "/img/download/" + file.getOriginalFilename());
+            return new Result<>(1, "图片上传成功", SERVER + "/img/download/" + file.getOriginalFilename());
         }
-        return new Result<String>(0, "图片上传失败");
+        return new Result<>(0, "图片上传失败");
     }
 
     @RequestMapping(value = "/download/{path}", method = RequestMethod.POST)
